d74e255a7fc738d03dac232ace9f96e24ee8fe13
Gabriel Massuia <gabrielmassuiaqueiroz@gmail.com>
2025-02-20 09:57:06 -0300

    carousel

--- src/components/services.tsx
+++ src/components/services.tsx
@@ -39,12 +39,44 @@ const services = [
   ]
 
 export function Services() {
+    const [emblaRef, emblaApi] = useEmblaCarousel({
+        loop: false,
+        align: "start",
+        slidesToScroll: 1,
+        breakpoints:{
+            "(min-width: 768px)": {slidesToScroll: 3}
+        }
+    })
     return (
         <section className="bg-white py-16">
             <div className="container mx-auto px-4">
                     <h2 className="text-4xl font-bold mb-12">
                         Serviços
                     </h2>
+                <div className="relative">
+                    <div className="overflow-hidden" ref={emblaRef}>
+                        <div className='flex'>
+                            {services.map((item, index) => (
+                                <div key={index} className='flex-[0_0_100%] min-w-0 md:flex-[0_0_calc(100%/3)] px-3'>
+                                    <article className='bg-[#1e293b] text-white rounded-2xl p-6 space-y-4 h-full flex flex-col'>
+                                        <div className='flex-1 flex items-start justify-between'>
+                                            <div className='flex gap-3'>
+                                                <span className='text-3xl'>{item.icon}</span>
+                                                <div>
+                                                    <h3 className='font-bold text-xl my-1'>{item.title}</h3>
+                                                    <p className='text-gray-400 text-sm select-none'>
+                                                        {item.description}
+                                                    </p>
+                                                </div>
+                                            </div>  
+                                        </div>
+                                    </article>
+
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     )
