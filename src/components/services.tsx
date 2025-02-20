5322e313f63b2e55a02e39456e2c9a4f777ffba1
Gabriel Massuia <gabrielmassuiaqueiroz@gmail.com>
2025-02-20 09:28:45 -0300

    carousel

--- src/components/services.tsx
+++ src/components/services.tsx
@@ -1,13 +1,15 @@
+"use client"
+import useEmblaCarousel from 'embla-carousel-react'
+import { ChevronLeft, ChevronRight, Scissors, Syringe, CarTaxiFront, Hotel } from 'lucide-react'
+import { WhatsappLogo } from '@phosphor-icons/react'
 
 export function Services() {
     return (
         <section className="bg-white py-16">
             <div className="container mx-auto px-4">
-                <div>
-                    <h2 className="text-4xl font-bold">
+                    <h2 className="text-4xl font-bold mb-12">
                         Serviços
                     </h2>
-                </div>
             </div>
         </section>
     )
