7e114ebce919456ae958e021e86d2e12b6f10488
Gabriel Massuia <gabrielmassuiaqueiroz@gmail.com>
2025-02-25 11:27:34 -0300

    services

--- src/components/services.tsx
+++ src/components/services.tsx
@@ -47,6 +47,13 @@ export function Services() {
             "(min-width: 768px)": {slidesToScroll: 3}
         }
     })
+
+    function scrollPrev() {
+        emblaApi?.scrollPrev()
+    }
+    function scrollNext() {
+        emblaApi?.scrollNext()
+    }
     return (
         <section className="bg-white py-16">
             <div className="container mx-auto px-4">
@@ -71,13 +78,13 @@ export function Services() {
                                             </div>  
                                         </div>
                                         <div className='border-t border-gray-700 pt-4 flex items-center justify-between'>
-                                            <div className='flex items-center gap-2 text-sm'>
+                                            <div className='flex items-center gap-2 text-sm select-none'>
                                                 <Clock className='w-4 h-4'/>
                                                 <span>{item.duration}</span>
                                             </div>
                                             <a 
                                                 href="#"
-                                                className='flex items-center justify-center gap-2 hover:bg-red-500 px-4 py-1 rounded-md duration-300'
+                                                className='flex items-center justify-center gap-2 hover:bg-red-500 px-4 py-1 rounded-md duration-300 select-none'
                                             >
                                                 Entrar em contato
                                                 <WhatsappLogo className='w-5 h-5'/>
@@ -88,6 +95,18 @@ export function Services() {
                             ))}
                         </div>
                     </div>
+                    <button 
+                        onClick={scrollPrev}
+                        className='bg-white flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute left-3 -translate-y-1/2 -translate-x-1/2 top-1/2 z-10'
+                    >
+                        <ChevronLeft className='w-6 h-6 text-gray-600'/>
+                    </button>
+                    <button 
+                        onClick={scrollNext}
+                        className='bg-white flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute -right-7 -translate-y-1/2 -translate-x-1/2 top-1/2 z-10'
+                    >
+                        <ChevronRight className='w-6 h-6 text-gray-600'/>
+                    </button>
                 </div>
             </div>
         </section>
